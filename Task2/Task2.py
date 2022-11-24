f1def61d42b3313bb6576bdfc2734e5769f3805f
Dmitrii Mironov <dimamironov@mail.ru>
2022-11-25 01:50:33 +0300

    Task 4 completed

--- Task2/Task2.py
+++ Task2/Task2.py
@@ -10,11 +10,11 @@ os.system('cls')
 
 print('Проверим истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z.')
 
-x = int(input('Введите число X:'))
+x = int(input('Введите число X: '))
 
-y = int(input('Введите число Y:'))
+y = int(input('Введите число Y: '))
 
-z = int(input('Введите число Z:'))
+z = int(input('Введите число Z: '))
 
 if (not(x or y or z) == (not(x)) and (not(y)) and (not(z))):
     print('Утверждение истинно!')
